32d9edef4dae64c6f3e441ce5f4dc5ccf44b5299
Francisco-24 <80161384+Francisco-24@users.noreply.github.com>
2022-10-19 22:00:38 +0100

    mais um

--- Projeto/Projeto_b2D.m
+++ Projeto/Projeto_b2D.m
@@ -46,13 +46,13 @@ csi
 ksi_x = [0.4782 3.2185 6.3224  9.4510 12.5861 15.7237 18.8627];
 ksi_y = [0.6510 3.2911 6.3610  9.4771 12.6057 15.7395 18.8758];
 
-x = linspace(0, 1, 20);
-y = linspace(0.5, 1, 20);
+x = linspace(0, 1, 6);
+y = linspace(0.5, 1, 7);
 theta_estrela_x = 0;
 theta_estrela_y = 0;
 
 %theta_estrela = zeros(6,6);
-
+% figure()
 for i=1:length(x)
     for j=1:length(y)
         for p=1:7
@@ -61,10 +61,16 @@ for i=1:length(x)
             theta_estrela_x = theta_estrela_x + C_x(p)*exp(-ksi_x(p)^2*alpha*500/(H/2)^2)*cos(ksi_x(p)*x(i));
             theta_estrela_y = theta_estrela_y + C_y(p)*exp(-ksi_y(p)^2*alpha*500/(W)^2)*cos(ksi_y(p)*y(j));
         end
-        theta_estrela(i,j) = theta_estrela_x*theta_estrela_y;
+        theta_estrela(j,i) = theta_estrela_x*theta_estrela_y;
+        theta_teste(j) = theta_estrela_x*theta_estrela_y;
         theta_estrela_x = 0;
         theta_estrela_y = 0;
     end
+%     plot(y, theta_teste, '.', 'markersize', 20)
+%     legend({'x=0','x=0.2','x=0.4','x=0.6','x=0.8','x=1'},'Location','northeast','Orientation','vertical')
+%     ylabel("$\theta*$", 'Interpreter','latex', 'FontSize', 18)
+%     xlabel("y [adimensional]", 'FontSize', 12)
+%     hold on
 end
 
 figure()
@@ -72,7 +78,7 @@ plot(y, theta_estrela, '.', 'markersize', 20)
 legend({'x=0','x=0.2','x=0.4','x=0.6','x=0.8','x=1'},'Location','northeast','Orientation','vertical')
 ylabel("$\theta*$", 'Interpreter','latex', 'FontSize', 18)
 xlabel("y [adimensional]", 'FontSize', 12)
-close
+
 
 figure()
 surf(x, y, theta_estrela);
@@ -81,53 +87,53 @@ xlabel("x*", 'FontSize', 12)
 ylabel("y*", 'FontSize', 12)
 zlabel("$\theta*$", 'Interpreter','latex', 'FontSize', 18)
 
-x=[0 1];
-y=[0.5 1];
-theta_estrela = 0;
-
-t = linspace(0, 8000, 10);
-for i=1:2
-    for k=1:2
-        for j=1:10
-            for p=1:7
-                C_x(p) = 4*sin(ksi_x(p))/(2*ksi_x(p) + sin(2*ksi_x(p)));
-                C_y(p) = 4*sin(ksi_y(p))/(2*ksi_y(p) + sin(2*ksi_y(p)));
-                theta_estrela_x = theta_estrela_x + C_x(p)*exp(-ksi_x(p)^2*alpha*t(j)/(H/2)^2)*cos(ksi_x(p)*x(i));
-                theta_estrela_y = theta_estrela_y + C_y(p)*exp(-ksi_y(p)^2*alpha*t(j)/(W)^2)*cos(ksi_y(p)*y(k));
-            end
-        theta_estrela(j) = theta_estrela_x*theta_estrela_y;
-        theta_estrela_x = 0;
-        theta_estrela_y = 0;
-        end
-        figure()
-        plot(t, theta_estrela, '.', 'markersize', 20)
-        legend(sprintf('x = %g, y = %g', x(i), y(k)),'Location','northeast','Orientation','vertical')
-        ylabel("$\theta*$", 'Interpreter','latex', 'FontSize', 18)
-        xlabel("t", 'FontSize', 12)
-        close
-    end
-end
-
-t = linspace(0, 8000, 200);
-for i=1:2
-    for k=1:2
-            for p=1:7
-                C_x(p) = 4*sin(ksi_x(p))/(2*ksi_x(p) + sin(2*ksi_x(p)));
-                C_y(p) = 4*sin(ksi_y(p))/(2*ksi_y(p) + sin(2*ksi_y(p)));
-                theta_estrela_x = theta_estrela_x + C_x(p)*exp(-ksi_x(p)^2*alpha.*t/(H/2)^2)*cos(ksi_x(p)*x(i));
-                theta_estrela_y = theta_estrela_y + C_y(p)*exp(-ksi_y(p)^2*alpha.*t/(W)^2)*cos(ksi_y(p)*y(k));
-            end
-        theta_estrela = theta_estrela_x.*theta_estrela_y;
-        theta_estrela_x = 0;
-        theta_estrela_y = 0;
-        figure()
-        plot(t, theta_estrela)
-        legend(sprintf('x = %g, y = %g', x(i), y(k)),'Location','northeast','Orientation','vertical')
-        ylabel("$\theta*$", 'Interpreter','latex', 'FontSize', 18)
-        xlabel("t", 'FontSize', 12)
-        hold on
-    end
-end
+% x=[0 1];
+% y=[0.5 1];
+% theta_estrela = 0;
+% 
+% t = linspace(0, 8000, 10);
+% for i=1:2
+%     for k=1:2
+%         for j=1:10
+%             for p=1:7
+%                 C_x(p) = 4*sin(ksi_x(p))/(2*ksi_x(p) + sin(2*ksi_x(p)));
+%                 C_y(p) = 4*sin(ksi_y(p))/(2*ksi_y(p) + sin(2*ksi_y(p)));
+%                 theta_estrela_x = theta_estrela_x + C_x(p)*exp(-ksi_x(p)^2*alpha*t(j)/(H/2)^2)*cos(ksi_x(p)*x(i));
+%                 theta_estrela_y = theta_estrela_y + C_y(p)*exp(-ksi_y(p)^2*alpha*t(j)/(W)^2)*cos(ksi_y(p)*y(k));
+%             end
+%         theta_estrela(j) = theta_estrela_x*theta_estrela_y;
+%         theta_estrela_x = 0;
+%         theta_estrela_y = 0;
+%         end
+%         figure()
+%         plot(t, theta_estrela, '.', 'markersize', 20)
+%         legend(sprintf('x = %g, y = %g', x(i), y(k)),'Location','northeast','Orientation','vertical')
+%         ylabel("$\theta*$", 'Interpreter','latex', 'FontSize', 18)
+%         xlabel("t", 'FontSize', 12)
+%         close
+%     end
+% end
+% 
+% t = linspace(0, 8000, 200);
+% for i=1:2
+%     for k=1:2
+%             for p=1:7
+%                 C_x(p) = 4*sin(ksi_x(p))/(2*ksi_x(p) + sin(2*ksi_x(p)));
+%                 C_y(p) = 4*sin(ksi_y(p))/(2*ksi_y(p) + sin(2*ksi_y(p)));
+%                 theta_estrela_x = theta_estrela_x + C_x(p)*exp(-ksi_x(p)^2*alpha.*t/(H/2)^2)*cos(ksi_x(p)*x(i));
+%                 theta_estrela_y = theta_estrela_y + C_y(p)*exp(-ksi_y(p)^2*alpha.*t/(W)^2)*cos(ksi_y(p)*y(k));
+%             end
+%         theta_estrela = theta_estrela_x.*theta_estrela_y;
+%         theta_estrela_x = 0;
+%         theta_estrela_y = 0;
+%         figure()
+%         plot(t, theta_estrela)
+%         legend(sprintf('x = %g, y = %g', x(i), y(k)),'Location','northeast','Orientation','vertical')
+%         ylabel("$\theta*$", 'Interpreter','latex', 'FontSize', 18)
+%         xlabel("t", 'FontSize', 12)
+%         hold on
+%     end
+% end
 
 
    
--- Projeto/Projeto_b3D.m
+++ Projeto/Projeto_b3D.m
@@ -21,7 +21,7 @@ Bi_y = h*L_y/k;
 Bi_z = h*L_z/k;
 
 %% alinea b) - cálculo dos csi's
-Bi = [Bi_x Bi_y Bi_z];
+Bi = [Bi_x Bi_y Bi_z]
 
 
 for l=1:3
@@ -50,7 +50,7 @@ ksi_y = [0.6510 3.2911 6.3610 9.4771 12.6057 15.7395 18.8758 22.0137 25.1525 28.
 ksi_z = [1.2011 3.8228 6.7156 9.7330 12.8039 15.9005 19.0112 22.1303 25.2548 28.3831];
 
 x = linspace(0, 1, 6);
-y = linspace(0.5, 1, 6);
+y = linspace(0.5, 1, 20);
 z = linspace(0, 1, 5);
 theta_estrela_x = 0;
 theta_estrela_y = 0;
@@ -58,6 +58,7 @@ theta_estrela_z = 0;
 
 %for em que para cada gráfico existe um único z
 for k=1:length(z)
+%     figure()
     for i=1:length(x)
         for j=1:length(y)
             for p=1:length(ksi_x)
@@ -68,11 +69,17 @@ for k=1:length(z)
                 theta_estrela_y = theta_estrela_y + C_y(p)*exp(-ksi_y(p)^2*alpha*500/(W)^2)*cos(ksi_y(p)*y(j));
                 theta_estrela_z = theta_estrela_z + C_z(p)*exp(-ksi_y(p)^2*alpha*500/(L/2)^2)*cos(ksi_z(p)*z(k));
             end
-            theta_estrela(i,j) = theta_estrela_x*theta_estrela_y*theta_estrela_z;
+            theta_estrela(j,i) = theta_estrela_x*theta_estrela_y*theta_estrela_z;
+            theta_teste(j) = theta_estrela_x*theta_estrela_y;
             theta_estrela_x = 0;
             theta_estrela_y = 0;
             theta_estrela_z = 0;
         end
+%         plot(y, theta_teste, '.', 'markersize', 20)
+%         legend({'x=0','x=0.2','x=0.4','x=0.6','x=0.8','x=1'},'Location','northeast','Orientation','vertical')
+%         ylabel("$\theta*$", 'Interpreter','latex', 'FontSize', 18)
+%         xlabel("y [adimensional]", 'FontSize', 12)
+%         hold on
     end
 
 figure()
@@ -86,7 +93,7 @@ end
 x = [0 1];
 y = [0 0.5 1];
 z = linspace(0, 1, 40);
-t = linspace(1, 1000, 5);
+t = linspace(1, 3000, 5);
 theta_estrela_x = 0;
 theta_estrela_y = 0;
 theta_estrela_z = 0;
